52a19ebf3d87a41763e755abda5279aa92143afc
filza <filza@gmail.com>
2019-08-26 19:30:29 +0500

    add my files

---
+++ contracts/news.sol
@@ -0,0 +1,17 @@
+pragma solidity >=0.4.0 <=0.6.0;
+
+contract news{
+	struct newsfeed{
+		address publisher;
+		string newsdesc;
+	}
+	mapping(uint => newsfeed) public newsfeeds;
+	uint public newsCount;
+
+	function addnews(string memory newsdesc) public {
+		newsCount++;
+		newsfeeds[newsCount].publisher = msg.sender;
+		newsfeeds[newsCount].newsdesc = newsdesc;
+
+	}
+}
